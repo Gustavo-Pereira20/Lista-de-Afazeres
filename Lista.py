203af0af82b69c492c20457ed7ea3baf326b31c4
Gustavo Pereira <gustavo.h.pereira13@aluno.senai.br>
2025-08-01 11:22:06 -0300

    aaaa

--- Lista.py
+++ Lista.py
@@ -1,5 +1,84 @@
-Listadetarefas = []
+
+escolha = 0
+primeira = input("Olá, gostaria de fazer uma lista de afazeres? (S/N) ").upper()
+if primeira == "S":
+    Listadetarefas = []
 while True:
-    tarefa = input("teste")
-    Listadetarefas.append(tarefa)
-    print (Listadetarefas)
+
+    if escolha == 0:
+        folha =print ("""    ____________________________________
+    |                                  |
+    |      Nova lista de Afazeres      | 
+    |__________________________________|
+    |                                  |
+    |   1. Inserir Tarefas             |
+    |                                  |
+    |   2. Listar Tarefas              |
+    |                                  |
+    |   3. Marcar como Concluído       |
+    |                                  |
+    |   4. Excluir Tarefas             |
+    |                                  |
+    |   5. Sair                        |
+    |__________________________________|
+    """)
+    escolha = int(input("O quê você deseja fazer?"))
+    if escolha == 1:
+        tarefa = input("Tudo bem, o quê gostaria de adicionar na lista de tarefas? ")
+        while True:
+            Listadetarefas.append(tarefa)
+            continuar = input("Quer adicionar mais alguma coisa? (S/N) ").upper()
+            if continuar == "N":
+                print("Tudo bem!")
+                escolha = 0
+                break
+            else:
+                tarefa = input("O quê você gostaria de adicionar na lista de tarefas? ")
+
+
+    elif escolha == 2:
+        c = 0
+        for i in Listadetarefas:
+            print (f" {c}. {i}")
+            c += 1
+        input ("Aperte ENTER para continuar.")
+        escolha = 0
+
+
+    elif escolha == 3:
+        c = 0
+        for i in Listadetarefas:
+            print (f" {c}. {i} ")
+            c += 1
+        pergunta = (input("Gostaria de concluir algum item da lista? (S/N) ")).upper()
+        if pergunta == "S":
+            n = int(input("Qual item?"))
+            Listadetarefas[n] = Listadetarefas[n] + " ☑"
+            print ("Item marcado!")
+        else:
+            escolha = 0
+
+
+    elif escolha == 4:
+        c = 0
+        for i in Listadetarefas:
+            print (f" {c}. {i} ")
+            c += 1
+        pergunta = (input("Gostaria de excluir algum item da lista? (S/N) ")).upper()
+        if pergunta == "S":
+            n = int(input("Qual item? "))
+            Listadetarefas.pop(n)
+            c = 0
+            for i in Listadetarefas:
+                print (f" {c}. {i} ")
+                c += 1
+            print ("Item excluido!")
+            input ("Aperte ENTER para continuar.")
+            escolha = 0
+        else:
+            escolha = 0
+
+
+    elif escolha == 5:
+        print("Ok, até mais!")
+        quit()
