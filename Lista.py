6a45dc8e3155f2a15da3783e7c054cd156c16ab3
Gustavo Pereira <gustavo.h.pereira13@aluno.senai.br>
2025-08-08 10:30:04 -0300

    o

--- Lista.py
+++ Lista.py
@@ -3,93 +3,100 @@ from open import blocodenotas
 escolha = 0
 primeira = input("Olá, gostaria de fazer uma lista de afazeres? (S/N) ").upper()
 if primeira == "S":
+
     Listadetarefas = []
-while True:
 
-    if escolha == 0:
-        folha = print ("""    ____________________________________
-    |                                  |
-    |      Nova lista de Afazeres      | 
-    |__________________________________|
-    |                                  |
-    |   1. Inserir Tarefas             |
-    |                                  |
-    |   2. Listar Tarefas              |
-    |                                  |
-    |   3. Marcar como Concluído       |
-    |                                  |
-    |   4. Excluir Tarefas             |
-    |                                  |
-    |   5. Sair                        |
-    |__________________________________|
-    """)
-    escolha = int(input("O quê você deseja fazer? "))
-    if escolha == 1:
-        tarefa = input("Tudo bem, o quê gostaria de adicionar na lista de tarefas? ")
-        while True:
-            Listadetarefas.append(tarefa)
-            continuar = input("Quer adicionar mais alguma coisa? (S/N) ").upper()
-            if continuar == "N":
-                print("Tudo bem!")
-                escolha = 0
-                time.sleep(1)               
-                break
-            else:
-                tarefa = input("O quê você gostaria de adicionar na lista de tarefas? ")
+    while True:
+
+        if escolha == 0:
+            folha = print ("""    ____________________________________
+        |                                  |
+        |      Nova lista de Afazeres      | 
+        |__________________________________|
+        |                                  |
+        |   1. Inserir Tarefas             |
+        |                                  |
+        |   2. Listar Tarefas              |
+        |                                  |
+        |   3. Marcar como Concluído       |
+        |                                  |
+        |   4. Excluir Tarefas             |
+        |                                  |
+        |   5. Sair                        |
+        |__________________________________|
+        """)
+        escolha = int(input("O quê você deseja fazer? "))
+
+        if escolha == 1:
+            tarefa = input("Tudo bem, o quê gostaria de adicionar na lista de tarefas? ")
+            while True:
+                Listadetarefas.append(tarefa)
+                continuar = input("Quer adicionar mais alguma coisa? (S/N) ").upper()
+                if continuar == "N":
+                    print("Tudo bem!")
+                    escolha = 0
+                    time.sleep(1)               
+                    break
+                else:
+                    tarefa = input("O quê você gostaria de adicionar na lista de tarefas? ")
 
 
-    elif escolha == 2:
-        c = 0
-        for i in Listadetarefas:
-            print (f" {c}. {i}")
-            blocodenotas(Listadetarefas)
-            c += 1
-        input ("Aperte ENTER para continuar.")
-        escolha = 0
-        time.sleep(1)
-
-    elif escolha == 3:
-        c = 0
-        for i in Listadetarefas:
-            print (f" {c}. {i} ")
-            c += 1
-        pergunta = (input("Gostaria de concluir algum item da lista? (S/N) ")).upper()
-        if pergunta == "S":
-            n = int(input("Qual item? "))
-            Listadetarefas[n] = Listadetarefas[n] + " (Concluído!)"
-            print ("Item marcado!")
-            blocodenotas(Listadetarefas)
-            time.sleep(1)
-            escolha = 0
-        else:
+        elif escolha == 2:
+            c = 0
+            for i in Listadetarefas:
+                print (f" {c}. {i}")
+                blocodenotas(Listadetarefas)
+                c += 1
+            input ("Aperte ENTER para continuar.")
             escolha = 0
             time.sleep(1)
 
-
-    elif escolha == 4:
-        c = 0
-        for i in Listadetarefas:
-            print (f" {c}. {i} ")
-            c += 1
-        pergunta = (input("Gostaria de excluir algum item da lista? (S/N) ")).upper()
-        if pergunta == "S":
-            n = int(input("Qual item? "))
-            Listadetarefas.pop(n)
+        elif escolha == 3:
             c = 0
             for i in Listadetarefas:
                 print (f" {c}. {i} ")
                 c += 1
-            print ("Item excluido!")
-            blocodenotas(Listadetarefas)
-            input ("Aperte ENTER para continuar.")
-            escolha = 0
-            time.sleep(1)
-        else:
-            escolha = 0
-            time.sleep(1)
+            pergunta = (input("Gostaria de concluir algum item da lista? (S/N) ")).upper()
+            if pergunta == "S":
+                n = int(input("Qual item? "))
+                Listadetarefas[n] = Listadetarefas[n] + " (Concluído!)"
+                print ("Item marcado!")
+                blocodenotas(Listadetarefas)
+                time.sleep(1)
+                escolha = 0
+            else:
+                escolha = 0
+                time.sleep(1)
 
 
-    elif escolha == 5:
-        print("Ok, até mais!")
-        time.sleep(1)
-        quit()
+        elif escolha == 4:
+            c = 0
+            for i in Listadetarefas:
+                print (f" {c}. {i} ")
+                c += 1
+            pergunta = (input("Gostaria de excluir algum item da lista? (S/N) ")).upper()
+            if pergunta == "S":
+                n = int(input("Qual item? "))
+                Listadetarefas.pop(n)
+                c = 0
+                for i in Listadetarefas:
+                    print (f" {c}. {i} ")
+                    c += 1
+                print ("Item excluido!")
+                blocodenotas(Listadetarefas)
+                input ("Aperte ENTER para continuar.")
+                escolha = 0
+                time.sleep(1)
+            else:
+                escolha = 0
+                time.sleep(1)
+
+
+        elif escolha == 5:
+            print("Ok, até mais!")
+            time.sleep(1)
+            quit()
+
+else:
+    print ("Certo, tenha um ótimo dia!")
+    time.sleep(1)
